4919b9bd9e681de2973c469fe83b18ce60684c00
Dan Thompson <dsthomp1@uci.edu>
2016-11-10 19:00:19 -0800

    Ship built with working doors
    Remember to load the ship layout scene or start menu scene depending on
    which one you want to work on.

---
+++ Assets/scripts/Game.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+	//used for primary functionality of the game
+	
+	GameObject door;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//if the left mouse button has been pressed
+		if(Input.GetMouseButtonDown(0)){
+			Doors.toggle_doors_on_click(door); 
+		}
+	}
+}
+
---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	//keep track of if an instance of the game manager is runing
+	public static GameManager instance = null;
+
+	//Don't code in here use Game.cs
+}
